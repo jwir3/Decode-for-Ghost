84fe36d10f6f4ca9f11bc04ee49b79d939cc942d
Scott Johnson <jaywir3@gmail.com>
2015-10-03 21:57:13 -0500

    Added Mono Social Icon Fonts, restyled to allow for more things on the icon bar at the bottom of the page, and added Disqus to posts.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,12 @@
 $( document ).ready(function() {
     $("article.post").fitVids();
+
+    /* Organize all of our social and subscription icons */
+    console.log("***** DEBUG_jwir3: Running main.js");
+    var numChildren = $(".icon-bar").children().length;
+    var i = 0;
+    $(".icon-bar").children().each(function() {
+      $(this).css('left', (i * 100 / (numChildren-1)) + "%");
+      i++;
+    });
 });
